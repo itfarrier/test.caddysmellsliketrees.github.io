21e3ab301ed8ffa2f0c8444345d17e613f6d7a33
Dmitrij Podabed <d.podabed@belitsoft.com>
2018-10-18 15:04:51 +0300

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-9b760cc0b8fe492eebb8.js","5c07d6f8007c68d3b07beaa1856dddf0"],["/app-d669784b8bee76ceef11.js","7c909ea9008f95d7088e784f5bc34851"],["/commons-481a4c35d1c7058beb12.js","4f1d09732f9340ffa8921103dd16c9ef"],["/index.html","5c26297eaa3175bb104b0afa7718f2da"],["/manifest.webmanifest","b528fc4c521ae75fab69cb4026149e08"],["/offline-plugin-app-shell-fallback/index.html","0466ce4246d1311f28776b1b7db0fa71"]];
+var precacheConfig = [["/app-9b760cc0b8fe492eebb8.js","5c07d6f8007c68d3b07beaa1856dddf0"],["/app-d352ad2c254f28c495f4.js","733a77d3339702ec7aeafeadd7322ab6"],["/app-d669784b8bee76ceef11.js","7c909ea9008f95d7088e784f5bc34851"],["/commons-481a4c35d1c7058beb12.js","4f1d09732f9340ffa8921103dd16c9ef"],["/index.html","04ba1e1e7233b9ad693aeb102c1c52eb"],["/manifest.webmanifest","b528fc4c521ae75fab69cb4026149e08"],["/offline-plugin-app-shell-fallback/index.html","b91cd4466dc2809279d5970089d8ed67"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
